bbfe10d8b44e99ac25af7a100e9fe4f0f2d0f8c0
meme5y <arthur874066@gmail.com>
2025-12-13 09:25:12 +0200

    Update pulse-detection.h

--- firmware/pulse-detection.h
+++ firmware/pulse-detection.h
@@ -1,1 +1,14 @@
+// pulse_detection.h
+// Header file for pulse detection algorithms
 
+#ifndef PULSE_DETECTION_H
+#define PULSE_DETECTION_H
+
+#include <Arduino.h>
+
+// Function declarations
+void setupPulseDetection();
+int calculateBPM(int pulses, unsigned long interval);
+bool isValidPulse(unsigned long lastPulseTime);
+
+#endif
